e7ec19f5e3b59482960b4cadd71a95de83079686
mymiracle0118 <noahsflood908@gmail.com>
2024-10-08 04:36:02 +0000

    modify middleward file

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,52 @@
-import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function middleware() {
   const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${ nonce }' 'strict-dynamic' 'unsafe-inline';
-    style-src 'self' 'nonce-${ nonce }' 'unsafe-inline' https://fonts.googleapis.com;
-    font-src 'self' https://fonts.gstatic.com;
+    script-src 'self' 'nonce-${ nonce }' 'strict-dynamic' ${ process.env.NODE_ENV === 'development' ? '\'unsafe-eval\'' : ''
+};
+    style-src 'self' 'nonce-${ nonce }';
     img-src 'self' blob: data:;
-    connect-src 'self';
+    font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
+    block-all-mixed-content;
     upgrade-insecure-requests;
 `;
-  // Replace newline characters and spaces
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, ' ')
-    .trim();
 
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set('x-nonce', nonce);
+  const responseHeaders = new Headers();
 
-  requestHeaders.set(
+  // Response headers
+  responseHeaders.set(
     'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue,
+    // Replace newline characters and spaces
+    cspHeader.replace(/\s{2,}/g, ' ').trim(),
   );
 
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
+  return NextResponse.next({
+    headers: responseHeaders,
   });
-  response.headers.set(
-    'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue,
-  );
-
-  return response;
 }
+
+export const config = {
+  matcher: [
+
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
+};
