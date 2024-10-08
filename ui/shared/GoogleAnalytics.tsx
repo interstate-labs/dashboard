8c6756fb4a7796fd98ad467e981eaca500c4e06e
mymiracle0118 <noahsflood908@gmail.com>
2024-10-08 04:30:29 +0000

    fix csp nonce

--- ui/shared/GoogleAnalytics.tsx
+++ ui/shared/GoogleAnalytics.tsx
@@ -14,8 +14,8 @@ const GoogleAnalytics = () => {
 
   return (
     <>
-      <Script strategy="lazyOnload" src={ `https://www.googletagmanager.com/gtag/js?id=${ id }` }/>
-      <Script strategy="lazyOnload" id="google-analytics">
+      <Script strategy="lazyOnload" nonce="<%= nonce %>" src={ `https://www.googletagmanager.com/gtag/js?id=${ id }` }/>
+      <Script strategy="lazyOnload" nonce="<%= nonce %>" id="google-analytics">
         { `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
