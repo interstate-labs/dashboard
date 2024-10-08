cb8095f40707988c1d12c44277017fb8f9749c46
mymiracle0118 <noahsflood908@gmail.com>
2024-10-08 03:08:30 +0000

    fix csp config

--- next.config.js
+++ next.config.js
@@ -72,23 +72,37 @@ const moduleExports = {
 };
 
 function generateCSP() {
-  const scriptSrc = process.env.NEXT_PUBLIC_CSP_SCRIPT_SRC || '\'self\' \'unsafe-inline\' \'unsafe-eval\'';
-  const connectSrc = process.env.NEXT_PUBLIC_CSP_CONNECT_SRC || '\'self\'';
+  // const scriptSrc = process.env.NEXT_PUBLIC_CSP_SCRIPT_SRC || '\'self\' \'unsafe-inline\' \'unsafe-eval\'';
+  // const connectSrc = process.env.NEXT_PUBLIC_CSP_CONNECT_SRC || '\'self\'';
+
+  // return `
+  //   default-src 'self';
+  //   script-src ${ scriptSrc };
+  //   connect-src ${ connectSrc };
+  //   style-src 'self' 'unsafe-inline';
+  //   img-src 'self' data: https:;
+  //   font-src 'self';
+  //   object-src 'none';
+  //   base-uri 'self';
+  //   form-action 'self';
+  //   frame-ancestors 'none';
+  //   block-all-mixed-content;
+  //   upgrade-insecure-requests;
+  // `.replace(/\s{2,}/g, ' ').trim();
 
   return `
-    default-src 'self';
-    script-src ${ scriptSrc };
-    connect-src ${ connectSrc };
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' data: https:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    block-all-mixed-content;
-    upgrade-insecure-requests;
-  `.replace(/\s{2,}/g, ' ').trim();
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data:;
+      font-src 'self';
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      upgrade-insecure-requests;
+      `.replace(/\n/g, '');
+
 }
 
 module.exports = withBundleAnalyzer(withRoutes(moduleExports));
